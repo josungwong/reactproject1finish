a00aa5d9790fb909a7591d8fac05e9931e656259
josungwong <jsw13160@gmail.com>
2024-08-13 14:18:18 +0900

    pm 02:18

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import Box from "./compo/Box"
+import {useState} from "react"
 
+const choice = {
+  rock:{
+    name:"rock",
+    img:"https://m.media-amazon.com/images/I/51rOMX5z40L.jpg"
+  },
+  scissors:{
+    name:"scissors",
+    img:"https://m.media-amazon.com/images/I/810Tz4tXUyL.jpg"
+  },
+  paper:{
+    name:"paper",
+    img:"https://m.media-amazon.com/images/I/617B4LbYLnL._AC_UF894,1000_QL80_.jpg"
+  }
+}
 function App() {
+  const [userSelect,setUserSelect] = useState(null)
+  const [computerSelect,setComputerSelect] = useState(null)
+  const [result,setResult] = useState("")
+
+  const play = (userChoice) => {
+    setUserSelect(choice[userChoice])  
+    let computerChoice = randomChoice()
+    setComputerSelect(computerChoice)
+    setResult(judgement(choice[userChoice],computerChoice))
+
+  }
+
+  const judgement = (user,cpu) => {
+    if(user.name === cpu.name){
+      return "Tie"
+    }else if(user.name === "rock")return cpu.name === "scissors"?"Win":"Lose"
+    else if(user.name === "scissors")return cpu.name === "paper"?"Win":"Lose"
+    else if(user.name === "paper")return cpu.name === "rock"?"Win":"Lose"
+  }
+
+  const randomChoice = () => {
+    let itemArray = Object.keys(choice) //객체의 키값만 뽑아서 배열로 만들어줌
+    let randomItem =Math.floor(Math.random()*itemArray.length)
+    let final = itemArray[randomItem]
+    return choice[final]
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='main'>
+        <Box title='You' item={userSelect} result={result}/>
+        <Box title='Computer' item={computerSelect} result={result}/>
+      </div>
+      <div className='main'>
+        <button onClick={()=>play("scissors")} className='buttons'>가위</button>
+        <button onClick={()=>play("rock")} className='buttons'>바위</button>
+        <button onClick={()=>play("paper")} className='buttons'>보</button>
+      </div>
     </div>
+    
   );
 }
 
---
+++ src/compo/Box.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+
+
+
+const Box = (ect) => {
+    let result
+    if(ect.title === "Computer" && ect.result !== "" && ect.result !== "Tie"){
+      result = ect.result=="Win"?"Lose":"Win"
+    }else{
+      result = ect.result
+    }
+  return (
+    <div className={`box ${result}`}>
+        <h1>{ect.title}</h1>
+        <img src={ect.item && ect.item.img} alt='none' className='item-img'/>
+        <h2>{result}</h2>
+    </div>
+  )
+}
+
+export default Box
